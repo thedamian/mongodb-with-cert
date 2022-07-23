f96441243165621869afeeb3d44c6adfbaad5149
Damian Montero <photoca@hotmail.com>
2022-07-22 21:05:21 -0400

    mongodb with cert

---
+++ mongodb.js
@@ -0,0 +1,26 @@
+const { MongoClient } = require("mongodb");
+// Replace the uri string with your connection string.
+// Replace the following with values for your environment.
+const username = encodeURIComponent("username");
+const clusterUrl = "xxxx.xxxx.mongodb.net";
+const clientPEMFile = encodeURIComponent("./X509-cert.pem");
+const authMechanism = "MONGODB-X509";
+// Replace the following with your MongoDB deployment's connection string.
+const uri =
+  `mongodb+srv://${username}@${clusterUrl}/?authMechanism=${authMechanism}&tls=true&tlsCertificateKeyFile=${clientPEMFile}`;
+
+const client = new MongoClient(uri);
+
+const database = client.db('sample_mflix');
+const movies = database.collection('movies');
+// Query for a movie that has the title 'Back to the Future'
+const query = { title: 'Back to the Future' };
+const movie =  movies.findOne(query, (err,movie) => {
+  if (err) {
+    console.error("ERROR: ",err)
+  }
+  console.log(movie);
+  client.close();
+});
+
+
